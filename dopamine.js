bf0bca15351db5dabf4616716e12ad0643974e65
Armin Dressler <adressler@web.de>
2011-11-22 11:43:17 +0100

    add interface for $.Callbacks

--- dopamine.js
+++ dopamine.js
@@ -25,6 +25,7 @@
 			options = options || {};
 
 			this.plugins = [];
+			this.callbacks = {};
 			this.options = $.extend({}, default_options, options);
 		},
 
@@ -43,6 +44,21 @@
 			});
 		},
 
+		addCallback: function(type, arg) {
+			var map = {};
+			if (typeof(type) == 'object') {
+				map = type;
+			} else {
+				map[type] = arg;
+			}
+
+			var self = this;
+			$.each(map, function(type, func) {
+				self.callbacks[type] = self.callbacks[type] || $.Callbacks();
+				self.callbacks[type].add(func);
+			});
+		},
+
 		acceptsType: function(type) {
 			var whitelisted = (this.options.accept.length == 0) || (this.options.accept.length > 0 && this.options.accept.indexOf(type) > -1);
 			var blacklisted = this.options.ignore.length > 0 && this.options.ignore.indexOf(type) > -1;
@@ -56,22 +72,26 @@
 
 			arg = arg || null;
 
-			// call events
-			$(this).trigger(type, [type, arg])
-			$(this).trigger('default', [type, arg]);
-
-			// call methods
+			// call events, callbacks and method
+			this.__call_event(type, type, arg);
+			this.__call_callback(type, type, arg);
 			this.__call_method(type, type, arg);
+
+			// call 'default' events, callbacks and method
 			if (type != 'default') {
+				this.__call_event('default', type, arg);
+				this.__call_callback('default', type, arg);
 				this.__call_method('default', type, arg);
 			}
 
 			// call plugins
-			$.each(this.plugins, function(i, plugin) {
-				plugin.call(type, arg);
-			});
+			this.__call_plugins(type, type, arg);
 
 			return true;
+		},
+
+		__call_event: function(method, type, arg) {
+			$(this).trigger(method, [type, arg])
 		},
 
 		__call_method: function(method, type, arg) {
@@ -81,8 +101,21 @@
 				return true;
 			}
 			return false;
-		}
+		},
 
+		__call_callback: function(method, type, arg) {
+			if (typeof(this.callbacks[method]) == 'object') {
+				this.callbacks[method].fire(type, arg);
+				return true;
+			}
+			return false;
+		},
+
+		__call_plugins: function(type, arg) {
+			$.each(this.plugins, function(i, plugin) {
+				plugin.call(type, arg);
+			});
+		}
 	}
 
 	Dopamine.extend = function(prototype) {
